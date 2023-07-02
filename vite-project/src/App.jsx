b0bdbcf1cad0cc7d048e212c5fe0297a46519c81
Mazen Saeed <104124933+Imazen7@users.noreply.github.com>
2023-07-02 15:16:22 +0530

    Initial changes

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import TodoWrapper from "./components/TodoWrapper"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <TodoWrapper />
+    </div>
+    </>
+  )
+}
+
+export default App
